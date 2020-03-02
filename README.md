8029c300d0cce7be2002779d17c1ff2d86dfc36f
OliviaL18 <oliviatloos@gmail.com>
2020-03-01 21:37:40 -0500

    after initial test of README.md file creation

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# ReadMe-Generator
 
-Construct a ReadMe file for a GitHub repository according to user input via the command prompt. 
+# Project Title
+DESCRIPTION TEXT HERE
+## Table of Contents
+*Installation
+*Usage
+*License
+*Contributers
+*Tests
+*Questions 
+## Installation
+What things you need to install the software and how to install them
+>Give examples
+Steps to get it going
+>step
+>step
+## Usage
+something here about using 
+## License
+This project is licensed under the MIT License (put something better here)
+## Contributing
+When contributing to this repository, please first discuss the change you wish to make via issue, email, or any other method with the owners of this repository before making a change.
+## Tests
+How to run tests for this system?
+## Contact
+* **NAME HERE** - (github url here)
+github image
+github email
---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "What is your GitHub Username?",
+            name: "github",
+        },
+
+    ])
+    .then(answers => {
+        const githubUsername = answers.github;
+
+        const readmeCode = `
+# Project Title
+DESCRIPTION TEXT HERE
+## Table of Contents
+*Installation
+*Usage
+*License
+*Contributers
+*Tests
+*Questions 
+## Installation
+What things you need to install the software and how to install them
+>Give examples
+Steps to get it going
+>step
+>step
+## Usage
+something here about using 
+## License
+This project is licensed under the MIT License (put something better here)
+## Contributing
+When contributing to this repository, please first discuss the change you wish to make via issue, email, or any other method with the owners of this repository before making a change.
+## Tests
+How to run tests for this system?
+## Contact
+* **NAME HERE** - (github url here)
+github image
+github email`;
+        fs.writeFile("README.md", readmeCode, function(err){
+            if(err){
+                throw err;
+            }
+            console.log("successfully created README");
+        });
+    });
